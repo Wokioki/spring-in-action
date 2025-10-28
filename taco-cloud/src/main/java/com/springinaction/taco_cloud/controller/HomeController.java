01ad6e0ed2e3347f58bc983f18d6d0255bad63a2
Wokioki <dtatuyko@gmail.com>
2025-10-28 02:07:55 +0100

    add controller

---
+++ taco-cloud/src/main/java/com/springinaction/taco_cloud/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.springinaction.taco_cloud.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+}

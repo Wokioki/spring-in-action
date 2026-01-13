f93b3fcc106eeca0b27e8967e5222a302f5f77ff
Wokioki <dtatuyko@gmail.com>
2026-01-13 01:06:53 +0100

    Reactor Flux boolean operators tests

---
+++ taco-cloud/src/test/java/com/springinaction/taco_cloud/reactor/FluxBooleanOperatorsTest.java
@@ -0,0 +1,38 @@
+package com.springinaction.taco_cloud.reactor;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class FluxBooleanOperatorsTest {
+
+    @Test
+    public void all() {
+        Flux<String> animalFlux = Flux.just(
+                "aardvark", "elephant", "koala", "eagle", "kangaroo");
+        Mono<Boolean> hasAMono = animalFlux.all(a -> a.contains("a"));
+        StepVerifier.create(hasAMono)
+                .expectNext(true)
+                .verifyComplete();
+        Mono<Boolean> hasKMono = animalFlux.all(a -> a.contains("k"));
+        StepVerifier.create(hasKMono)
+                .expectNext(false)
+                .verifyComplete();
+    }
+
+    @Test
+    public void any() {
+        Flux<String> animalFlux = Flux.just(
+                "aardvark", "elephant", "koala", "eagle", "kangaroo");
+        Mono<Boolean> hasTMono = animalFlux.any(a -> a.contains("t"));
+        StepVerifier.create(hasTMono)
+                .expectNext(true)
+                .verifyComplete();
+        Mono<Boolean> hasZMono = animalFlux.any(a -> a.contains("z"));
+        StepVerifier.create(hasZMono)
+                .expectNext(false)
+                .verifyComplete();
+    }
+
+}
